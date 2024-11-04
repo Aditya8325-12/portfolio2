fbf8ba2e973ca190e4d009931be45f47b462364e
Aditya8325-12 <68269361+Aditya8325-12@users.noreply.github.com>
2024-11-04 14:17:48 +0000

    Deploying to gh-pages from @ Aditya8325-12/portfolio2@5eae91ad7c5fe8a8be0eecca65ca50d240299750 🚀

---
+++ _app/immutable/chunks/experience.72da1b7e.js
@@ -0,0 +1,7 @@
+import{A as e}from"./UIcon.ccef43d7.js";import{g as t}from"./skills.faaf3a3e.js";import{C as o}from"./types.585195e1.js";const s=[{slug:"open-sourcer",company:" V4U Tracking Solutions Limited",description:`Software Developer Intern
+		V4U Tracking Solutions Limited
+		
+		- Developed and deployed mobile apps for iOS and Android using React Native.
+		- Integrated APIs, resolved issues, and optimized performance.
+		- Published apps on the Apple Store and Google Play Store.
+		- Built responsive websites with React, Tailwind CSS, and Bootstrap.`,contract:o.FullTime,type:"Software Development",location:"Aurangabad Maharashtra",period:{from:new Date(2023,9,1),to:new Date(2024,4,0)},skills:t("react","react-native","js","tailwindcss","bootstrap","ts"),name:" Software Developer Intern",color:"red",links:[],logo:e.v4u,shortDescription:""}],n="Experience";export{s as i,n as t};
